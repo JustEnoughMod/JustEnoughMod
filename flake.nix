ff6449ca69042e3f10aea153fe182acab76e14c8
LDprg <lukas_4dr@gmx.at>
2024-01-24 18:36:23 +0100

    add bevy template

--- flake.nix
+++ flake.nix
@@ -33,21 +33,11 @@
         craneLib = crane.lib.${system};
         src = craneLib.cleanCargoSource (craneLib.path ./.);
 
-        # Common arguments can be set here to avoid repeating them later
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
+        buildDeps = (with pkgs; [ pkg-config makeWrapper clang mold ]);
 
-          buildInputs = [
-            # Add additional build inputs here
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
-          ];
-
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
-        };
+        runtimeDeps = (with pkgs;
+          [ libxkbcommon alsa-lib udev vulkan-loader wayland ]
+          ++ (with xorg; [ libXcursor libXrandr libXi libX11 ]));
 
         craneLibLLvmTools = craneLib.overrideToolchain
           (fenix.packages.${system}.complete.withComponents [
@@ -56,17 +46,26 @@
             "rustc"
           ]);
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
         # Build the actual crate itself, reusing the dependency
         # artifacts from above.
-        my-crate = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-        });
-      in
-      {
+        my-crate = craneLib.buildPackage rec {
+          pname = "JustEnoughMod";
+          src = ./.;
+
+          nativeBuildInputs = buildDeps;
+          buildInputs = runtimeDeps;
+
+          postInstall = ''
+            wrapProgram $out/bin/${pname} \
+              --prefix LD_LIBRARY_PATH : ${
+                pkgs.lib.makeLibraryPath runtimeDeps
+              } \
+              --prefix XCURSOR_THEME : "Adwaita"
+            mkdir -p $out/bin/assets
+            cp -a assets $out/bin
+          '';
+        };
+      in {
         checks = {
           # Build the crate as part of `nix flake check` for convenience
           inherit my-crate;
@@ -77,58 +76,35 @@
           # Note that this is done as a separate derivation so that
           # we can block the CI if there are issues here, but not
           # prevent downstream consumers from building our crate by itself.
-          my-crate-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
+          my-crate-clippy = craneLib.cargoClippy {
             cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
-
-          my-crate-doc = craneLib.cargoDoc (commonArgs // {
-            inherit cargoArtifacts;
-          });
+          };
 
           # Check formatting
-          my-crate-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
+          my-crate-fmt = craneLib.cargoFmt { inherit src; };
 
           # Audit dependencies
-          my-crate-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
+          my-crate-audit = craneLib.cargoAudit { inherit src advisory-db; };
 
           # Audit licenses
-          my-crate-deny = craneLib.cargoDeny {
-            inherit src;
-          };
+          my-crate-deny = craneLib.cargoDeny { inherit src; };
 
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `my-crate` if you do not want
-          # the tests to run twice
-          my-crate-nextest = craneLib.cargoNextest (commonArgs // {
-            inherit cargoArtifacts;
-            partitions = 1;
-            partitionType = "count";
-          });
         };
 
         packages = {
           default = my-crate;
-        } // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-          my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
-            inherit cargoArtifacts;
-          });
         };
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = my-crate;
-        };
+        apps.default = flake-utils.lib.mkApp { drv = my-crate; };
 
         devShells.default = craneLib.devShell {
           # Inherit inputs from checks.
           checks = self.checks.${system};
 
           # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath runtimeDeps}";
+          XCURSOR_THEME = "Adwaita";
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
