f85ac1c67456a5b388a91cc0369e65ec8d0de810
LDprg <lukas_4dr@gmx.at>
2024-02-23 21:56:03 +0100

    add pre compiled headers

--- include/core/Logger.hpp
+++ include/core/Logger.hpp
@@ -15,35 +15,35 @@ namespace JEM
         constexpr auto trace(spdlog::format_string_t<Args...> fmt, Args &&...args) -> void
         {
             return m_logger->trace(std::forward<spdlog::format_string_t<Args...>>(fmt),
-                                               std::forward<Args>(args)...);
+                                   std::forward<Args>(args)...);
         }
 
         template <typename... Args>
         constexpr auto debug(spdlog::format_string_t<Args...> fmt, Args &&...args) -> void
         {
             return m_logger->debug(std::forward<spdlog::format_string_t<Args...>>(fmt),
-                                               std::forward<Args>(args)...);
+                                   std::forward<Args>(args)...);
         }
 
         template <typename... Args>
         constexpr auto info(spdlog::format_string_t<Args...> fmt, Args &&...args) -> void
         {
             return m_logger->info(std::forward<spdlog::format_string_t<Args...>>(fmt),
-                                               std::forward<Args>(args)...);
+                                  std::forward<Args>(args)...);
         }
 
         template <typename... Args>
         constexpr auto warn(spdlog::format_string_t<Args...> fmt, Args &&...args) -> void
         {
             return m_logger->warn(std::forward<spdlog::format_string_t<Args...>>(fmt),
-                                               std::forward<Args>(args)...);
+                                  std::forward<Args>(args)...);
         }
 
         template <typename... Args>
         constexpr auto error(spdlog::format_string_t<Args...> fmt, Args &&...args) -> void
         {
             return m_logger->error(std::forward<spdlog::format_string_t<Args...>>(fmt),
-                                               std::forward<Args>(args)...);
+                                   std::forward<Args>(args)...);
         }
 
     private:
