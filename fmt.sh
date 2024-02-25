b0d1c2220828ff509643dade10423e84e1b83281
LDprg <lukas_4dr@gmx.at>
2024-02-25 16:51:46 +0100

    add precommit to repo

--- fmt.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-find ./src -name "*.cpp" -o -name "*.hpp" | sed 's| |\\ |g' | xargs clang-format -i
-find ./include -name "*.cpp" -o -name "*.hpp" | sed 's| |\\ |g' | xargs clang-format -i
